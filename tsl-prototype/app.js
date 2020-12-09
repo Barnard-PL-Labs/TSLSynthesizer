44ebc5fb279d6aa69b50181bdbb4e3f1513a8a4c
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2020-12-08 19:11:20 -0500

    add rudimentary upload feature

--- tsl-prototype/app.js
+++ tsl-prototype/app.js
@@ -1,24 +1,31 @@
-var audioContext;
+//get input by id
+var audioFile = document.getElementById("file")
+//get source by id
+var audioSource = document.getElementById("audioSource");
+//get audio by id
+var audioTag = document.getElementById('audio');
 
-var modulatorFreq;
-var modulationIdx;
+//apply event listener to input using the playAudio function
+audioFile.addEventListener("change", function() {
+  playAudio(this);
+});
 
-function init() {
-    audioContext = new(window.AudioContext || window.webkitAudioContex);
-    var osc = audioContext.createOscillator();
-    modulatorFreq = audioContext.createOscillator();
 
-    modulationIdx = audioContext.createGain();
-    modulationIdx.gain.value = 100;
-    modulatorFreq.frequency.value = 100;
-
-    modulatorFreq.connect(modulationIdx);
-    modulationIdx.connect(osc.frequency);
-
-    osc.connect(audioContext.destination);
-
-    osc.start();
-    modulatorFreq.start();
+function playAudio(input) {
+  //empty var to read file
+  var reader;
+  //if a file is uploaded then use FileReader to read file
+  if (input.files && input.files[0]) {
+    reader = new FileReader();
+    //onload set the audio source with the uploaded audio
+    reader.onload = function(e) {
+      audioSource.setAttribute('src', e.target.result);
+      //load source
+      audioTag.load();
+      //play source
+      audioTag.play();
+    }
+    //read contents of of file
+    reader.readAsDataURL(input.files[0]);
+  }
 }
-
-// init();
