5ee1e4c5cede4301d4483449d4ce3451cee9858a
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2020-12-13 13:03:26 +0900

    add psuedocode for hardcoded tsl spec circuit example

--- tsl-prototype/app.js
+++ tsl-prototype/app.js
@@ -1,16 +1,54 @@
-//get input by id
-var trackA = document.getElementById("trackAFile")
-//get source by id
-var audioSource = document.getElementById("audioSource");
-//get audio by id
-var audioTag = document.getElementById('audio');
+/////////////////////
+//  Specification  //
+///////////////////// 
 
-//button to press play
-var playButton = document.getElementById('playBtn')
+///////////////////
+//  Track Files  //
+/////////////////// 
 
+let trackA = document.getElementById("trackAFile");
+let trackB = document.getElementById("trackBFile");
+let trackC = document.getElementById("trackCFile");
+let trackD = document.getElementById("trackDFile");
+
+/////////////////////
+//  Mixer Buttons  //
+///////////////////// 
+
+let buttonA = document.getElementById("btnA");
+
+/////////////////////
+//  Program Logic  //
+///////////////////// 
+
+let playButton = document.getElementById('playBtn');
+let currPlaying = False;
+let audioOutput;
+
+// Manages play/pause and audio update based on event listeners
 playButton.addEventListener("click", function() {
-  playAudio(trackA);
-})
+  if(!currPlaying){
+    let program = hardCodedProgram();
+    currPlaying = True;
+    anyKey.addEventListener("onclick", audioOutput = program.audioOutput());
+  }
+  else{
+    currPlaying = False;
+    audioOutput.stop();
+  }
+});
+
+// Make this a class
+function hardCodedProgram() {
+  let track2Playing = False;
+  buttonA.addEventListener("onclick", function(){track2Playing = True;});
+  function audioOutput() {
+    if(!track2Playing)
+      return trackA;
+    else
+      return trackB;
+  }
+}
 
 /*
   make sure to have an event listener, but to depend on the actions of the circuit
@@ -18,10 +56,16 @@ playButton.addEventListener("click", function() {
   - the function/circuit should start when we press play
   -                             stop  when we press pause
 
+  - learn how to get elements from spec.
+
   boil down to a very stripped down version of the app:
     - always Track1 until Track2
     - pressButton -> Track2
 */
+
+// Swap with webaudio later on
+let audioSource = document.getElementById("audioSource");
+let audioTag = document.getElementById('audio');
 
 function playAudio(input) {
   //empty var to read file
