a267b937c7aa8fcbc0ca634e3f4bc0a464e164bc
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2020-12-15 12:10:15 +0900

    hardcoded toy sample with 2 specs

--- tsl-prototype/app.js
+++ tsl-prototype/app.js
@@ -1,92 +1,4 @@
 /////////////////////
-//  Specification  //
-///////////////////// 
-
-///////////////////
-//  Track Files  //
-/////////////////// 
-
-let trackA = document.getElementById("trackAFile");
-let trackB = document.getElementById("trackBFile");
-let trackC = document.getElementById("trackCFile");
-let trackD = document.getElementById("trackDFile");
-let noTrack = Null; // TODO 
-
-/////////////////////
-//  Mixer Buttons  //
-///////////////////// 
-
-let buttonA = document.getElementById("btnA");
-let buttonB = document.getElementById("btnB");
-
-/////////////////////
-//  Program Logic  //
-///////////////////// 
-
-let playButton = document.getElementById('playBtn');
-let currPlaying = False;
-let audioOutput;
-
-// Manages play/pause and audio update based on event listeners
-playButton.addEventListener("click", function() {
-  if(!currPlaying){
-    let program = hardCodedProgram();
-    currPlaying = True;
-    anyKey.addEventListener("onclick", audioOutput = program.audioOutput());
-  }
-  else{
-    currPlaying = False;
-    audioOutput.stop();
-  }
-});
-
-////////////////////////
-//  Synthesized Code  //
-////////////////////////
-
-// Spec:
-// always Track1 until Track2
-// pressButton -> Track2
-// FIMXE: Make this a class
-function hardCodedProgram() {
-  let track2Playing = False;
-  buttonA.addEventListener("onclick", function(){track2Playing = True;});
-  function audioOutput() {
-    if(!track2Playing)
-      return trackA;
-    else
-      return trackB;
-  }
-}
-
-// Spec:
-// button1 --> play/pause
-// knob 1 --> volume
-// knob 2 --> freq
-// trackA and trackB
-// trackB until trackC
-// buttonB -> play/pause trackB
-function hardCodedProgram2(){
-  let isPlaying = Null; // Then isPlaying can change
-  // [isPlaying <- trackA] is predicate
-}
-
-// Uses DOM elements.
-function programFromSpecification(){
-}
-
-// Uses parameters.
-function programWithArgs(){
-}
-
-
-//////////////////////
-//  HTTP w/ Server  //
-//////////////////////
-
-
-
-/////////////////////
 //  Audio Control  //
 /////////////////////
 
@@ -96,7 +8,7 @@ let audioTag = document.getElementById('audio');
 
 function playAudio(input) {
   //empty var to read file
-  var reader;
+  let reader;
   //if a file is uploaded then use FileReader to read file
   if (input.files && input.files[0]) {
     reader = new FileReader();
@@ -112,3 +24,99 @@ function playAudio(input) {
     reader.readAsDataURL(input.files[0]);
   }
 }
+
+/////////////////////
+//  Specification  //
+///////////////////// 
+
+///////////////////
+//  Track Files  //
+/////////////////// 
+
+let trackA = document.getElementById("trackAFile");
+let trackB = document.getElementById("trackBFile");
+let trackC = document.getElementById("trackCFile");
+let trackD = document.getElementById("trackDFile");
+let noTrack = null;
+
+/////////////////////
+//  Mixer Buttons  //
+///////////////////// 
+
+let buttonA = document.getElementById("btnA");
+let buttonB = document.getElementById("btnB");
+
+
+/////////////////////
+//  Program Logic  //
+/////////////////////
+
+let playButton = document.getElementById('playBtn');
+let currPlaying = false;
+let program;
+
+// Manages play/pause and audio update based on event listeners
+playButton.addEventListener("click", function() {
+  if(!currPlaying){
+    program = new hardCodedProgram(playAudio);
+    currPlaying = true;
+    document.body.addEventListener("click", function(){program.play()});
+  }
+  else{
+    currPlaying = false;
+    audioTag.pause();
+  }
+});
+
+////////////////////////
+//  Synthesized Code  //
+////////////////////////
+
+let synthesized = document.getElementsByTagName("textarea")[0];
+
+const spec1 = "Always Track1 Until Track2\npressButtonA -> Track2"
+class hardCodedProgram{
+  constructor(playAudio){
+    this.track2Playing = false;
+    this.playAudio = playAudio;
+    buttonA.addEventListener("click", _ => this.track2Playing = true);
+    synthesized.value = spec1;
+  }
+  play(){
+    audioTag.pause();
+    if(!this.track2Playing)
+      playAudio(trackA);
+    else
+      playAudio(trackB);
+  }
+}
+
+
+// TODO
+/*
+
+// Spec:
+// button1 --> play/pause
+// knob 1 --> volume
+// knob 2 --> freq
+// trackA and trackB
+// trackB until trackC
+// buttonB -> play/pause trackB
+function hardCodedProgram2(){
+  let isPlaying = null; // Then isPlaying can change
+  // [isPlaying <- trackA] is predicate
+}
+
+// Uses DOM elements.
+function programFromSpecification(){
+}
+
+// Uses parameters.
+function programWithArgs(){
+}
+ */
+
+
+//////////////////////
+//  HTTP w/ Server  //
+//////////////////////
