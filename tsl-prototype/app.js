44d649f014849a2460cf07961134811b503b9ac3
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2020-12-23 22:57:40 +0900

    finish prototype

--- tsl-prototype/app.js
+++ tsl-prototype/app.js
@@ -1,57 +1,16 @@
 /////////////////////
-//  Audio Control  //
-/////////////////////
-
-let audioCtx;
-const audioGain = 0.1;
-let switchVal = 0;
-
-async function loadBuffer(bufferURL){
-  const response = await fetch(bufferURL);
-  const arrayBuffer = await response.arrayBuffer();
-  return await audioCtx.decodeAudioData(arrayBuffer);
-}
-
-async function initWebAudio(){
-  audioCtx = new (window.AudioContext || window.webkitAudioContext);
-  let audioSource = await loadBuffer('./sample.mp3');
-  let source = audioCtx.createBufferSource();
-  source.buffer = audioSource;
-
-  let gainNode = audioCtx.createGain();
-  gainNode.gain.value = audioGain;
-  source.connect(gainNode);
-  gainNode.connect(audioCtx.destination);
-  source.loop = true;
-  source.start();
-
-  let audioSrc2 = await loadBuffer('./sample2.mp3');
-  let src2 = audioCtx.createBufferSource();
-  src2.buffer = audioSrc2;
-  src2.connect(audioCtx.destination);
-  src2.start();
-}
-
-document.getElementById('btnA').addEventListener("click", async function(){
-  await initWebAudio();
-})
-
-document.getElementById('btnB').addEventListener("click", function(){
-  if(switchVal % 2 === 0){
-    audioCtx.suspend();
-    switchVal ++;
-  }
-  else{
-    audioCtx.resume();
-    switchVal ++;
-  }
-}, false);
-
-/////////////////////
 //  Specification  //
 /////////////////////
 
-// TODO: get spec from DOM
+// TODO: use
+const spec1Head = document.getElementById('1-predicate-fst');
+console.log(spec1Head).value;
+
+//////////////////////
+//  HTTP w/ Server  //
+//////////////////////
+const synthesized = "foobarbaz";
+document.getElementsByTagName("textarea")[0].value = synthesized;
 
 ///////////////////
 //  Track Files  //
@@ -72,34 +31,10 @@ let buttonB = document.getElementById("btnB");
 let knobA = document.getElementById("knobA");
 let knobB = document.getElementById("knobB");
 
-
-/////////////////////
-//  Program Logic  //
-/////////////////////
-
-let playButton = document.getElementById('playBtn');
-let currPlaying = false;
-let program;
-
-// Manages play/pause and audio update based on event listeners
-playButton.addEventListener("click", function() {
-  if(!currPlaying){
-    program = new ReactiveControl();
-    currPlaying = true;
-    document.body.addEventListener("click", function(){program.play()});
-  }
-  else{
-    currPlaying = false;
-    program.stop();
-  }
-});
-
 ////////////////////////
 //  Synthesized Code  //
 ////////////////////////
-
-let synthesized = document.getElementsByTagName("textarea")[0];
-spec= `
+spec = `
   --- Full TSL Spec ---
   // buttonA => play/stop audio
   press buttonA && [audio <- play] -> X [audio <- stop];
@@ -116,68 +51,160 @@ spec= `
   [trackB <- play] W [trackC <- play];
 `
 
-// TODO
-function p_press(foo){}
-function play(){return true;}
-function stop(){return false;}
+// Implemented functions
+function btnPressed(btnBool) {
+  return btnBool;
+}
 
-let o_audio  = true,
+function play() {
+  return true;
+}
+
+function stop() {
+  return false;
+}
+
+// Initialize cells and outputs
+let o_audio = true,
     o_trackA = true,
     o_trackB = true,
     o_trackC = false,
-    c_audio  = o_audio,
+    c_audio = o_audio,
     c_trackA = o_trackA,
     c_trackB = o_trackB,
     c_trackC = o_trackC;
 
 // Human transpiled to js from kotlin.
-class ReactiveControl{
-  constructor(buttonA, buttonB){
-    this.btnAPressed = p_press(buttonA);
-    this.btnBPressed = p_press(buttonB);
-  }
-  returnOutput(){
-    let circuit = this.controlCircuit();
+function updateAudio(btnA, btnB) {
+  let btnAPressed = btnPressed(btnA);
+  let btnBPressed = btnPressed(btnB);
+  let circuit = this.controlCircuit(btnAPressed, btnBPressed);
 
-    c_audio  = o_audio;
-    c_trackA = o_trackA;
-    c_trackB = o_trackB;
-    c_trackC = o_trackC;
+  c_audio = o_audio;
+  c_trackA = o_trackA;
+  c_trackB = o_trackB;
+  c_trackC = o_trackC;
 
-    let stop = stop();
-    let play = play();
+  let stop = stop();
+  let play = play();
 
-    o_audio = this.audioSwitch([stop, circuit[7]],
-                               [play, circuit[8]],
-                               [c_audio, circuit[9]]);
-    o_trackA = this.trackASwitch([c_trackA, circuit[5]],
-                                 [play, circuit[6]]);
-    o_trackB = this.trackBSwitch([c_trackB, circuit[2]],
-                                 [stop, circuit[3]],
-                                 [play, circuit[4]]);
-    o_trackC = this.trackCSwitch([c_trackC, circuit[0]],
-                                 [play, circuit[1]]);
-  }
-  audioSwitch(stop, play, prev){
-    const temp = play[1] ? play[0] : prev[0];
-    return stop[1] ? stop[0] : temp;
-  }
-  trackASwitch(prev, play){
-    return prev[1] ? prev[0] : play[0]
-  }
-  trackBSwitch(prev, stop, play){
-    const temp = stop[1] ? stop[0] : play[0];
-    return prev[1] ? prev[0] : temp;
-  }
-  trackCSwitch(prev, play){
-    return prev[1] ? prev[0] : play[0];
-  }
-  controlCircuit(){
-    return [false, true, true, false, false, true, false, false, false, true];
-  }
+  o_audio = this.audioSwitch([stop, circuit[7]],
+      [play, circuit[8]],
+      [c_audio, circuit[9]]);
+  o_trackA = this.trackASwitch([c_trackA, circuit[5]],
+      [play, circuit[6]]);
+  o_trackB = this.trackBSwitch([c_trackB, circuit[2]],
+      [stop, circuit[3]],
+      [play, circuit[4]]);
+  o_trackC = this.trackCSwitch([c_trackC, circuit[0]],
+      [play, circuit[1]]);
 }
 
+function audioSwitch(stop, play, prev) {
+  const temp = play[1] ? play[0] : prev[0];
+  return stop[1] ? stop[0] : temp;
+}
 
-//////////////////////
-//  HTTP w/ Server  //
-//////////////////////
+function trackASwitch(prev, play) {
+  return prev[1] ? prev[0] : play[0]
+}
+
+function trackBSwitch(prev, stop, play) {
+  const temp = stop[1] ? stop[0] : play[0];
+  return prev[1] ? prev[0] : temp;
+}
+
+function trackCSwitch(prev, play) {
+  return prev[1] ? prev[0] : play[0];
+}
+
+function controlCircuit(btnAPressed, btnBPressed) {
+  return [false, true, true, false, false, true, false, false, false, true];
+}
+
+/////////////////////
+//  Audio Control  //
+/////////////////////
+
+let audioCtx;
+const audioGain = 0.1;
+let switchVal = 0;
+
+async function loadBuffer(bufferURL) {
+  const response = await fetch(bufferURL);
+  const arrayBuffer = await response.arrayBuffer();
+  return await audioCtx.decodeAudioData(arrayBuffer);
+}
+
+async function initWebAudio() {
+  audioCtx = new (window.AudioContext || window.webkitAudioContext);
+  let audioSource = await loadBuffer('./sample.mp3');
+  let source = audioCtx.createBufferSource();
+  source.buffer = audioSource;
+
+  let gainNode = audioCtx.createGain();
+  gainNode.gain.value = audioGain;
+  source.connect(gainNode);
+  gainNode.connect(audioCtx.destination);
+  source.loop = true;
+  source.start();
+
+  let audioSrc2 = await loadBuffer('./sample2.mp3');
+  let src2 = audioCtx.createBufferSource();
+  src2.buffer = audioSrc2;
+  src2.connect(audioCtx.destination);
+  src2.start();
+}
+
+document.getElementById('btnA').addEventListener("click", async function () {
+  await initWebAudio();
+})
+
+document.getElementById('btnB').addEventListener("click", function () {
+  if (switchVal % 2 === 0) {
+    audioCtx.suspend();
+    switchVal++;
+  } else {
+    audioCtx.resume();
+    switchVal++;
+  }
+}, false);
+
+/////////////////////
+//  Program Logic  //
+/////////////////////
+
+let playButton = document.getElementById('playBtn');
+let currPlaying = false;
+
+// Manages play/pause and audio update based on event listeners
+playButton.addEventListener("click", function() {
+  if(!currPlaying){
+    currPlaying = true;
+    react();
+  }
+  else{
+    currPlaying = false;
+  }
+});
+
+function react(){
+  buttonA.addEventListener("click", _ => updateAudio(true, false) && playAudio());
+  buttonB.addEventListener("click", _ => updateAudio(false, true) && playAudio());
+}
+
+// TODO: make fake circuit
+
+// TODO: implement this part
+function playAudio(){
+  if(o_audio){
+    if(o_trackA){}
+    else{}
+    if(o_trackB){}
+    else{}
+    if(o_trackC){}
+    else{}
+  }
+  else
+    audioCtx.suspend();
+}
--- tsl-prototype/toy-spec.kt
+++ tsl-prototype/toy-spec.kt
@@ -1,93 +1,75 @@
 // Module : Toy-spec
 //
-// Rxlotlin Interface for toy-spec.
+// JavaScript Interface for toy-spec.
 //
 
 /////////////////////////////////////////////////////////////////////////////
 
-module Toy-spec
-  ( toy-spec
-  ) where
+function toy-spec ( buttonA
+ , buttonB
+ ){
 
-/////////////////////////////////////////////////////////////////////////////
+      let w6 = f_play()
+      let w7 = f_stop()
+      let b8 = p_press(s_buttonA)
+      let b9 = p_press(s_buttonB)
 
-import Control.Arrow
-  ( Arrow
-  , ArrowLoop
-  , returnA
-  , arr
-  , (<<<)
-  )
-
------------------------------------------------------------------------------
-
-toy-spec
-data class ntupleb(val audio:b, val trackA:b, val trackB:b, val trackC:b)
-
-
-private fun toy-spec
-       ( buttonA: a
-       , buttonB: a
-       ):ntupleb{
-
-      val w6 = f_play()
-      val w7 = f_stop()
-      val b8 = p_press(s_buttonA)
-      val b9 = p_press(s_buttonB)
-
-    val cout = controlCircuit(b9, b8)
+     let innerCircuit = controlCircuit(b9, b8)
 
       c_audio = o_audio
       c_trackA = o_trackA
       c_trackB = o_trackB
       c_trackC = o_trackC
 
-      val o_audio = audioSwitch(Pair(w7, cout[7]), Pair(w6, cout[8]), Pair(c_audio, cout[9]))
+      o_audio = audioSwitch([w7, innerCircuit[8]], [w6, innerCircuit[9]], [c_audio, innerCircuit[10]])
 
-      val o_trackA = trackASwitch(Pair(w6, cout[5]), Pair(c_trackA, cout[6]))
+      o_trackA = trackASwitch([w6, innerCircuit[6]], [c_trackA, innerCircuit[7]])
 
-      val o_trackB = trackBSwitch(Pair(w7, cout[2]), Pair(w6, cout[3]), Pair(c_trackB, cout[4]))
+      o_trackB = trackBSwitch([w7, innerCircuit[3]], [w6, innerCircuit[4]], [c_trackB, innerCircuit[5]])
 
-      val o_trackC = trackCSwitch(Pair(w6, cout[0]), Pair(c_trackC, cout[1]))
+      o_trackC = trackCSwitch([w7, innerCircuit[0]], [w6, innerCircuit[1]], [c_trackC, innerCircuit[2]])
 
-    return ntuple(o_audio, o_trackA, o_trackB, o_trackC)
+
 }
 
-+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-private fun <T>audioSwitch
-       ( p0: Pair<T,Boolean>
-       , p1: Pair<T,Boolean>
-       , p2: Pair<T,Boolean>
-       ):T{
-    val r1 = if (p1.second) p1first else p2.first 
-    val r0 = if (p0.second) p0first else r1
-     return r0
+/////////////////////////////////////////////////////////////////////////////
+
+function audioSwitch
+       ( p0
+       , p1
+       , p2
+       ){
+    const r1 = p1[1] ? p1[0] : p2;[0]; 
+    const r0 = p0[1] ? p0[0] : r1;
+     return r0;
 }
 
-private fun <T>trackASwitch
-       ( p0: Pair<T,Boolean>
-       , p1: Pair<T,Boolean>
-       ):T{
-    val r0 = if (p0.second) p0first else p1.first 
-    return r0
+function trackASwitch
+       ( p0
+       , p1
+       ){
+    const r0 = p0[1] ? p0[0] : p1;[0]; 
+    return r0;
 }
 
-private fun <T>trackBSwitch
-       ( p0: Pair<T,Boolean>
-       , p1: Pair<T,Boolean>
-       , p2: Pair<T,Boolean>
-       ):T{
-    val r1 = if (p1.second) p1first else p2.first 
-    val r0 = if (p0.second) p0first else r1
-     return r0
+function trackBSwitch
+       ( p0
+       , p1
+       , p2
+       ){
+    const r1 = p1[1] ? p1[0] : p2;[0]; 
+    const r0 = p0[1] ? p0[0] : r1;
+     return r0;
 }
 
-private fun <T>trackCSwitch
-       ( p0: Pair<T,Boolean>
-       , p1: Pair<T,Boolean>
-       ):T{
-    val r0 = if (p0.second) p0first else p1.first 
-    return r0
+function trackCSwitch
+       ( p0
+       , p1
+       , p2
+       ){
+    const r1 = p1[1] ? p1[0] : p2;[0]; 
+    const r0 = p0[1] ? p0[0] : r1;
+     return r0;
 }
 
 
@@ -95,10 +77,22 @@ private fun controlCircuit
     ( cin0: Boolean
     , cin1: Boolean
     ): Array<Boolean>{
+      w3 <- _lat_ <<< _not_ -< w3
+      w4 <- _lat_ -< w5
 
+        w5 = !w4 && w3
+        w6 = w4 && w3
+        o0 = !w3
+        o4 = !w3
+        o6 = !w6
+        o9 = !w3
 
-    return arrayOf(true, false, false, false, true, true, false, false, false, true)
+    return arrayOf(o0, w5, false, w5, o4, false, o6, false, w5, o9, false)
 
  }
 
-=============================================================================
+  where
+    _lat_ = cell False
+    _not_ = arr not
+
+
