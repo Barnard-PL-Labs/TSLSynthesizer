a5336b45f753067c709398e1ed59faa7775cee69
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-01-07 02:04:52 +0900

    rehaul specification UI

--- public/htmlControl.js
+++ public/htmlControl.js
@@ -1,40 +1,151 @@
-///////////////////////////////////////
-//  INITIALIZE SPECIFICATION FIELD   //
-///////////////////////////////////////
+///////////////////
+//  HTML FIELDS  //
+///////////////////
 
 NUM_SPECS = 4
 
-function specHtmlInit(){
+const predicateOptions = `
+<select class="predicateOption">
+    <option value=""></option> 
+    <option value="always">Always</option> 
+    <option value="play">Played Note:</option>
+    <option value="[waveform <- sine()]">waveform is sine</option>
+    <option value="[waveform <- sawtooth()]">waveform is sawtooth</option>
+    <option value="[waveform <- square()]">waveform is square</option>
+    <option value="[waveform <- triangle()]">waveform is triangle</option>
+    <option value="[amSynthesis <- True()]">AM on</option> 
+    <option value="[amSynthesis <- False()]">AM off</option> 
+    <option value="[fmSynthesis <- True()]">FM on</option> 
+    <option value="[fmSynthesis <- False()]">FM off</option> 
+    <option value="[lfo <- True()]">LFO on</option> 
+    <option value="[lfo <- False()]">LFO off</option>
+</select>
+`
+
+const actionOptions = `
+<select class="actionSelect">
+    <option value=""></option> 
+    <option value="[waveform <- sine()]">waveform to sine</option>
+    <option value="[waveform <- sawtooth()]">waveform to sawtooth</option>
+    <option value="[waveform <- square()]">waveform to square</option>
+    <option value="[waveform <- triangle()]">waveform to triangle</option>
+    <option value="[amSynthesis <- True()]">AM on</option> 
+    <option value="[amSynthesis <- False()]">AM off</option> 
+    <option value="[fmSynthesis <- True()]">FM on</option> 
+    <option value="[fmSynthesis <- False()]">FM off</option> 
+    <option value="[lfo <- True()]">LFO on</option> 
+    <option value="[lfo <- False()]">LFO off</option>
+</select> 
+`
+
+const untilOptions = `
+<select class="untilOption">
+    <option value=""></option> 
+    <option value="">No Condition</option> 
+    <option value="play">Played Note:</option>
+    <option value="[waveform <- sine()]">waveform to sine</option>
+    <option value="[waveform <- sawtooth()]">waveform to sawtooth</option>
+    <option value="[waveform <- square()]">waveform to square</option>
+    <option value="[waveform <- triangle()]">waveform to triangle</option>
+    <option value="[amSynthesis <- True()]">AM on</option> 
+    <option value="[amSynthesis <- False()]">AM off</option> 
+    <option value="[fmSynthesis <- True()]">FM on</option> 
+    <option value="[fmSynthesis <- False()]">FM off</option> 
+    <option value="[lfo <- True()]">LFO on</option> 
+    <option value="[lfo <- False()]">LFO off</option>
+</select>
+`
+
+const playNoteOptions = `
+<select class="playNoteOptions">
+    <option value=""></option> 
+    <option value="play">Selected Note</option>
+    <option value="playBelow">Above Selected Note</option>
+    <option value="playAbove">Below Selected Note</option>
+</select> 
+`
+
+// TODO:
+// CHANGE NAMING
+const binOperatorOptions = `
+<select class="binOp">
+    <option value=""></option> 
+    <option value=" -> ">Implies</option>
+    <option value=" <-> ">Equivalence</option>
+    <option value=" -> X ">Next</option>
+    <option value=" W ">Weak Until</option>
+    <option value=" U ">Until</option>
+</select> 
+`
+
+function strToDOM(html){
+    const domNode = document.createElement("template");
+    domNode.innerHTML = html.trim();
+    return domNode.content.firstChild;
+}
+
+function getSpanNode(innerText){
+    const spanNode = document.createElement("span");
+    spanNode.innerText = innerText;
+    return spanNode;
+}
+
+function getWhitespaceSpanNode(){
+    return getSpanNode("    ");
+}
+
+function getActionClauseList(){
+    return [
+        getWhitespaceSpanNode(),
+        strToDOM(actionOptions),
+        getSpanNode(" W "),
+        strToDOM(untilOptions)
+    ];
+}
+
+////////////////////////////
+//  DYNAMIC HTML LOADING  //
+////////////////////////////
+
+function nodesAfterPredicate(predicate){
+    let returnNodes = [];
+    switch(predicate.value){
+        case "":
+            return returnNodes;
+        case "always":
+            break;
+        case "play":
+            returnNodes.push(strToDOM(playNoteOptions));
+            returnNodes.push(getWhitespaceSpanNode());
+            returnNodes.push(getSpanNode("⇒"));
+            break;
+        default:
+            returnNodes.push(strToDOM(binOperatorOptions));
+            returnNodes.push(getWhitespaceSpanNode());
+    }
+    return returnNodes.concat(getActionClauseList());
+}
+
+
+///////////////////////////////////////////
+//  EVENT LISTENERS AND INITIALIZATIONS  //
+///////////////////////////////////////////
+
+
+function specificationHTMLInit(){
     const root = document.getElementById("specification");
     for(let i=0; i<NUM_SPECS; i++){
         const singleSpec = document.createElement('article');
         singleSpec.setAttribute("id", "spec-" + i.toString());
-        singleSpec.appendChild(createPredicateHTML());
-        singleSpec.appendChild(document.createTextNode("\t"));
+        singleSpec.appendChild(strToDOM(predicateOptions));
+        singleSpec.appendChild(getWhitespaceSpanNode());
         root.appendChild(singleSpec);
+        root.appendChild(document.createElement("br"));
     }
 }
 
-function createPredicateHTML(){
-    const predicate = document.createElement("select");
-    predicate.setAttribute("class", "predicate");
-    predicate.appendChild(document.createElement("option"));
-
-    const when = document.createElement("option");
-    when.setAttribute("value", "when");
-    when.innerText = "When..."
-    predicate.appendChild(when);
-
-    const always = document.createElement("option");
-    always.setAttribute("value", "always");
-    always.innerText = "Always"
-    predicate.appendChild(always);
-
-    return predicate;
-}
-
 function predEventListenerInit(){
-    const predicates = document.getElementsByClassName("predicate");
+    const predicates = document.getElementsByClassName("predicateOption");
     for(let i=0; i<predicates.length; i++){
         let currPred = predicates[i];
         let parent = currPred.parentNode;
@@ -46,93 +157,35 @@ function predEventListenerInit(){
 
             if(currPred.value === "")
                 return;
-            else if(currPred.value === "when") {
-                parent.appendChild(createPlayOptions());
-            }
 
-            parent.appendChild(document.createTextNode('\t'));
+            parent.appendChild(getWhitespaceSpanNode());
 
-            // Add action
-            let action = document.createElement("template");
-            action.setAttribute("id", "foo");
-            action.innerHTML = actionHTML.trim();
-            parent.appendChild(action.content);
-
-            parent.appendChild(document.createTextNode('\t'));
-            parent.appendChild(createUntilOptions());
+            const nodesAfter = nodesAfterPredicate(currPred);
+            for(let j=0; j<nodesAfter.length; j++)
+                parent.appendChild(nodesAfter[j]);
         })
     }
 }
 
-function createPlayOptions(){
-    const playNote = document.createElement("select");
-    playNote.setAttribute("class", "playNote");
-    playNote.appendChild(document.createElement("option"));
+function untilEventListener(event){
+    const currUntil = event.target;
+    const parent = currUntil.parentNode;
+    const lastSibling = parent.children[parent.children.length-1];
 
-    const exact = document.createElement("option");
-    exact.setAttribute("value", "exact");
-    exact.innerText = "Played selected note";
-    playNote.append(exact);
+    if(lastSibling.classList[0] === "playNoteOptions")
+        lastSibling.remove();
 
-    const below = document.createElement("option");
-    below.setAttribute("value", "below");
-    below.innerText = "Played below selected note";
-    playNote.append(below);
-
-    const above = document.createElement("option");
-    above.setAttribute("value", "above");
-    above.innerText = "Played above selected note";
-    playNote.append(above);
-
-    return playNote;
-}
-
-const actionHTML = `
-<select class="action">
-    <option value=""></option> 
-    <option value="sine">waveform to sine</option>
-    <option value="sawtooth">waveform to sawtooth</option>
-    <option value="square">waveform to square</option>
-    <option value="triangle">waveform to triangle</option>
-    <option value="amOn">AM on</option> 
-    <option value="amOff">AM off</option> 
-    <option value="fmOn">FM on</option> 
-    <option value="fmOff">FM off</option> 
-    <option value="lfoOn">LFO on</option> 
-    <option value="lfoOff">LFO off</option>
-</select> 
-until
-`
-
-function createUntilOptions(){
-    const playNote = document.createElement("select");
-    playNote.setAttribute("class", "until");
-    playNote.appendChild(document.createElement("option"));
-
-    const noCondition = document.createElement("option");
-    noCondition.setAttribute("value", "");
-    noCondition.innerText = "No condition";
-    playNote.append(noCondition);
-
-    const exact = document.createElement("option");
-    exact.setAttribute("value", "exact");
-    exact.innerText = "Played selected note";
-    playNote.append(exact);
-
-    const below = document.createElement("option");
-    below.setAttribute("value", "below");
-    below.innerText = "Played below selected note";
-    playNote.append(below);
-
-    const above = document.createElement("option");
-    above.setAttribute("value", "above");
-    above.innerText = "Played above selected note";
-    playNote.append(above);
-
-    return playNote;
+    if(currUntil.value === "play")
+        parent.appendChild(strToDOM(playNoteOptions));
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-    specHtmlInit();
+    specificationHTMLInit();
     predEventListenerInit();
 }, false);
+
+document.addEventListener("change", function(e){
+    if(e.target.className === "untilOption")
+        untilEventListener(e);
+})
+
