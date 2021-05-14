94a743435e775c6504f7ced7bbfd4a00dc39f32c
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-05-15 00:21:53 +0900

    add devtools

--- public/client.js
+++ public/client.js
@@ -123,3 +123,24 @@ function onMIDIFailure() {
     console.log('Error: Could not access MIDI devices.');
 
 }
+
+function genRandSpec(){
+	const noteBase = "note6";
+	for(let i=0; i<selectedNotesList.length; i++)
+	    selectedNotesList[i] = `${noteBase}${i}`;
+	randomSpecBtn.click();
+}
+
+function genManyRandSpecs(times){
+    const specs = [];
+    for(let i=0; i<times; i++){
+        genRandSpec();
+        specs.push(getSpecFromDOM());
+    }
+    return specs
+}
+
+function concatManyRandSpecs(times){
+	const specList = genManyRandSpecs(times);
+	return specList.join('\n***\n\n');
+}
