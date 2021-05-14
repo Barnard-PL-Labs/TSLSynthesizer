6de3b34435ecb87357df8b92650377027b62775c
Wonhyuk Choi <wc012013@mymail.pomona.edu>
2021-05-15 00:32:42 +0900

    update devtools

--- devtools/measure.py
+++ devtools/measure.py
@@ -1,7 +1,7 @@
 import os
 from tqdm import tqdm
 
-with open("specifications.txt", "r") as f:
+with open("specs.log", "r") as f:
     specs = f.read()
 
 spec_list = specs.split("\n***\n\n")
@@ -9,7 +9,7 @@ spec_list = specs.split("\n***\n\n")
 def run_once(spec):
     with open("temp.tsl", "w") as f:
         f.write(spec)
-    os.system("./synthesize.sh temp.tsl")
+    os.system("./timer.sh tmp.tsl")
 
 for single_spec in tqdm(spec_list):
     run_once(single_spec)
